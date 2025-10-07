282a57bb9fb1db7446c490982c7a27fe22100722
Yassin ROUIS <48293288+yassin-rouis@users.noreply.github.com>
2025-10-07 12:37:57 +0100

    Création d'un contrôleur 'article'

---
+++ controllers/articleController.js
@@ -0,0 +1,21 @@
+const Logger = require("../logger");
+
+const testAPI = (req, res) => {
+    res.status(200).json({
+        message: "Le test a bien fonctionné",
+        success: true
+    })
+}
+const createArticle = async (req, res) => {
+    const articleData = req.body;
+    Logger.success("Données reçues par le contrôleur : " + articleData);
+    res.status(201).json({
+        message: "Article créé avec succès via le contrôleur !",
+        article: articleData,
+        success: true
+    })
+}
+
+module.exports = {
+    testAPI, createArticle
+}
